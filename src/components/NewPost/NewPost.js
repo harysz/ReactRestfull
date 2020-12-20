7e6449aac635e7639154a8b3bb809991068b6951
harysz <arnodlas.valasinas@gmail.com>
2020-12-20 13:29:46 +0200

    adding posts

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,5 +1,7 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
 
+import axios from 'axios';
 import './NewPost.css';
 
 class NewPost extends Component {
@@ -8,7 +10,9 @@ class NewPost extends Component {
         content: '',
         author: 'Max'
     }
-
+    postDataHandler =()=>{
+        axios.post();
+    }
     render () {
         return (
             <div className="NewPost">
@@ -22,7 +26,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
